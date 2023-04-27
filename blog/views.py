98e9e8ecdd809b4ce9aed544a9b1a2d50ff4b03f
oconnorian3 <oconnorian3@gmail.com>
2023-04-27 21:00:54 +0000

    Added home,html url to url.py file

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
 
+def home(request):
+    return render(request, 'blog/home.html')
