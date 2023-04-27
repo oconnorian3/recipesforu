629a9b5daf44eb1ee9cc105f8f9baa435b09ed63
oconnorian3 <oconnorian3@gmail.com>
2023-04-27 21:10:36 +0000

    Added comment functions and like functions to posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,1 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import generic, View
-from .models import Post
-
-def home(request):
-    return render(request, 'blog/home.html')
+from django.shortcuts import render
