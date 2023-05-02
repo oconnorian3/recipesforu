a4f0b344f6e039ff6df8afd008c60e8c596939a0
oconnorian3 <oconnorian3@gmail.com>
2023-05-02 09:05:31 +0000

    Added test code from other project to test page functionalities

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "homepage.html"
+    paginate_by = 6
+
 
 def homepage(request):
     return render(request, 'homepage.html')
