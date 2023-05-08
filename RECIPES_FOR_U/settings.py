510c5024a1b88bbd300dfafc957b8e0e8bae7934
oconnorian3 <oconnorian3@gmail.com>
2023-05-08 20:39:54 +0000

    Removed CRISPY_TEMPLATE_PACK = 'bootstrap4 as a test

--- RECIPES_FOR_U/settings.py
+++ RECIPES_FOR_U/settings.py
@@ -65,7 +65,7 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+#CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
