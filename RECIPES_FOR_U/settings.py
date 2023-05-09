84991ca3da8a3a4f7f222df56b3da3cffb4c51b8
oconnorian3 <oconnorian3@gmail.com>
2023-05-09 21:02:18 +0000

    Added cloudinary defines

--- RECIPES_FOR_U/settings.py
+++ RECIPES_FOR_U/settings.py
@@ -31,7 +31,6 @@ result = cloudinary.uploader.upload('static/css/style.css', resource_type='raw')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
@@ -58,12 +57,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.sites',
     'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'django.contrib.staticfiles',
     'django_summernote',
-    'cloudinary',
+    
     'bootstrap4',
     'crispy_forms',
     'crispy_bootstrap4',
@@ -162,14 +162,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATIC_URL = '/static/'
+#STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+#DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 
 
 # Default primary key field type
