9978c9c3e5ff6a993e161bee442ac22915974c8a
oconnorian3 <oconnorian3@gmail.com>
2023-05-15 18:32:19 +0000

    Removed commented out code from settings.py

--- RECIPES_FOR_U/settings.py
+++ RECIPES_FOR_U/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["recipesforu.herokuapp.com", "localhost"]
 
@@ -100,16 +100,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'RECIPES_FOR_U.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
 
 DATABASES = {
    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
